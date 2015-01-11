bc56f97eb8fc3a951f10a223106c26993f982ddc
timserck <timserck@gmail>
2015-01-11 15:00:37 +0100

    test

---
+++ _js/js.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+
+	var next = $('.next');
+	 
+	
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 0) {
+            next.fadeIn();
+        } else {
+            next.fadeOut();
+        }
+		});
+        
+    
+
+		next.click( function(event) { // Au clic sur un élément
+			
+			console.log('testingscrool');
+			var page = $(this).attr('href'); // Page cible
+			var speed = 800; // Durée de l'animation (en ms)
+			$('html, body').animate( { scrollTop: $(page).offset().top }, speed ); // Go
+			return false;
+		});
+	});
